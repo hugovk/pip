d444ea238459ddb9f98ad51775928a0f3233ce2c
Ellen Marie Dash <me@duckie.co>
2019-04-08 13:00:09 -0400

    Add 'pip cache' command.

--- src/pip/_internal/utils/filesystem.py
+++ src/pip/_internal/utils/filesystem.py
@@ -1,4 +1,5 @@
 import errno
+import fnmatch
 import os
 import os.path
 import random
@@ -17,7 +18,7 @@ from pip._internal.utils.misc import cast
 from pip._internal.utils.typing import MYPY_CHECK_RUNNING
 
 if MYPY_CHECK_RUNNING:
-    from typing import BinaryIO, Iterator
+    from typing import BinaryIO, Iterator, List
 
     class NamedTemporaryFileResult(BinaryIO):
         @property
@@ -167,3 +168,14 @@ def _test_writable_dir_win(path):
     raise EnvironmentError(
         'Unexpected condition testing for writable directory'
     )
+
+
+def find_files(path, pattern):
+    # type: (str, str) -> List[str]
+    """Returns a list of absolute paths of files beneath path, recursively,
+    with filenames which match the UNIX-style shell glob pattern."""
+    result = []  # type: List[str]
+    for root, dirs, files in os.walk(path):
+        matches = fnmatch.filter(files, pattern)
+        result.extend(os.path.join(root, f) for f in matches)
+    return result
